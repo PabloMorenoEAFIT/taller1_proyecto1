074baba8454ab6d68df5b1266f1d105318c6a92f
pmorenoq <pmorenoq@eafit.edu.co>
2023-08-27 23:49:58 -0500

    comit taller2

--- moviereviews/moviereviews/urls.py
+++ moviereviews/moviereviews/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for moviereviewsproject project.
+URL configuration for moviereviews project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 from movie import views as movieViews
-from about import views as aboutViews
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', movieViews.home),
+    path('', movieViews.home, name = 'home'),
     path('about/', movieViews.about),
     path('news/', include('news.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
